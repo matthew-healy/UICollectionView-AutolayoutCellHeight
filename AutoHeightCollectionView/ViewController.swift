0c142b051fc4476bc37175b5d35a189e0a688f66
Matthew Healy <matthew.healy@soundcloud.com>
2019-01-07 17:40:43 +0100

    Initial Commit

---
+++ AutoHeightCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoHeightCollectionView
+//
+//  Created by Matthew Healy on 07.01.19.
+//  Copyright © 2019 Matthew Liam Healy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
